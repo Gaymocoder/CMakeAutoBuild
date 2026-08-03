303e7bac50a2d4c32844c4e44319ed4cef9013a7
Gaymocoder <gaymocoder@gmail.com>
2026-08-03 06:23:48 +0300

    B&W | Updated clang and Visual Studio versions
    — Updated clang installation script;
    — Now it builds for clang-22 and VS 18 2026;
    — Fixed wrong version for GCC: from only major to full (14.2.0);
    
    — Builds and works properly.

--- .github/workflows/scripts/clang.sh
+++ .github/workflows/scripts/clang.sh
@@ -1,2 +1,8 @@
+wget https://apt.llvm.org/llvm.sh
+chmod +x llvm.sh
+sudo ./llvm.sh 22
 sudo apt update
-sudo apt install -y clang-17 libc++-17-dev libc++abi-17-dev
+sudo apt install -y libc++-22-dev libc++abi-22-dev
+
+sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-22 100
+sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-22 100
