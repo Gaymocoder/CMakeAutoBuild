02aaf9960fb7925c3391614296690b3ff175922f
Gaymocoder <gaymocoder@gmail.com>
2026-08-10 23:06:04 +0300

    B&W | Removed "test_werror_func" from Hello
    — Removed "test_werror_func"-function from Hello/src/main.cpp;
    — The "GCS_WERROR"-check in GitHub Actions is done;
    
    — Builds and works properly.

--- Hello/src/main.cpp
+++ Hello/src/main.cpp
@@ -5,12 +5,6 @@
 #include <vector>
 #include <format>
 
-int test_werror_func(int x)
-{
-    if (x == 1)
-        return 1;
-}
-
 int main(int argc, char** argv)
 {
     std::print("════════════════════════════════════════\n");
