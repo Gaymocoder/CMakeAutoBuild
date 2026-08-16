689b537e692a877edbfc693c33893d239a6e0ec4
Gaymocoder <gaymocoder@gmail.com>
2026-08-16 18:14:47 +0300

    B&W | Added "install_only"-files list
    — Split the updating list into "install_and_update" and "install_only";
    — "install_only"-files are copied only when absent in the superproject;
    
    — Cosmetics in ci reports:
      - Preset lines are marked with " - " instead of leading spaces;
      - Removed the blank line under the "— Builds with:"-header.

--- scripts/gcst_update.py
+++ scripts/gcst_update.py
@@ -5,39 +5,48 @@ from pathlib import Path
 GCST_NAME = "CMakeAutoBuild"
 HERE = Path(__file__).resolve().parent
 gcst_path = Path(subprocess.check_output(['git', '-C', HERE, 'rev-parse', '--show-toplevel'], text = True).strip()).absolute()
-superproject_path = Path(subprocess.check_output(['git', '-C', HERE, 'rev-parse', '--show-superproject-working-tree'], text = True).strip()).absolute()
+sproject_path = Path(subprocess.check_output(['git', '-C', HERE, 'rev-parse', '--show-superproject-working-tree'], text = True).strip()).absolute()
 
-files_to_update = [
+install_and_update = [
     "./.github/workflows",
     "./cmake",
     "./build.bat",
     "./build.sh",
-    "./CMakeLists.txt",
-    "./conanfile.py",
     "./configure.py",
     "./presets.json",
     "./scripts/gcst_update.py"
 ]
 
+install_only = [
+    "./conanfile.py",
+    "./CMakeLists.txt",
+]
+
+# ToDo: presets.local.json
+
 def get_updating_files():
-    global files_to_update, gcst_path
-    root = gcst_path
     all_files = []
+    files_to_update = install_and_update + install_only
     for entry in files_to_update:
         path = Path(gcst_path / entry).absolute()
         if not path.is_dir():
-            all_files.append(path.relative_to(root, walk_up = True))
+            all_files.append(path.relative_to(gcst_path, walk_up = True))
             continue
 
         for file in path.rglob("*"):
             if file.is_dir():
                 continue
-            all_files.append(file.relative_to(root, walk_up = True))
+            all_files.append(file.relative_to(gcst_path, walk_up = True))
 
-    matches, mismatches, errors = filecmp.cmpfiles(superproject_path, gcst_path, all_files, shallow=False)
-    all_files = [name for name in all_files if (name in (set(mismatches) | set(errors))) and ((gcst_path / name).exists())]
+    matches, mismatches, errors = filecmp.cmpfiles(sproject_path, gcst_path, all_files, shallow=False)
+    files_to_update = []
+    for name in all_files:
+        if (name in set(mismatches) | set(errors)) and (gcst_path/name).exists():
+            if (name in install_only) and (sproject_path/name).exists():
+                continue
+            files_to_update.append(name)
 
-    return all_files
+    return files_to_update
 
 
 def find_gcst_submodule():
@@ -51,8 +60,8 @@ def find_gcst_submodule():
 
 
 def main():
-    global superproject_path, gcst_path
-    if (superproject_path == gcst_path):
+    global sproject_path, gcst_path
+    if (sproject_path == gcst_path):
         gcst_path = find_gcst_submodule()
         if (gcst_path == ''):
             print("The gcstemplate is not a submodule of any repo. Merging impossible")
@@ -72,9 +81,9 @@ def main():
 
     for file in mismatches:
         ifile = gcst_path / file
-        ofile = superproject_path / file
+        ofile = sproject_path / file
 
-        print(f'Copying "./{(ifile).relative_to(superproject_path, walk_up = True)}" to "./{(ofile).relative_to(gcst_path, walk_up = True)}"')
+        print(f'Copying "./{(ifile).relative_to(sproject_path, walk_up = True)}" to "./{(ofile).relative_to(gcst_path, walk_up = True)}"')
         shutil.copy(ifile, ofile)
 
 if __name__ == '__main__':
